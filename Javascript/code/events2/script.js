89c816039699b5646356eda0d4d18b854561a5f2
prasad-bigdp <bigdp585@gmail.com>
2025-10-09 16:19:27 +0530

    update

---
+++ Javascript/code/events2/script.js
@@ -0,0 +1,4 @@
+document.getElementById('gc').addEventListener('click', () => alert("grand child clicked"),true);
+document.getElementById("c").addEventListener("click", () => alert(" child clicked"),true)
+document.getElementById("p").addEventListener("click", () => alert("parent clicked"),true)
+document.getElementById("gp").addEventListener("click", () => alert("grand parent clicked"),true)
